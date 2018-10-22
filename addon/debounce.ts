aa21238099cb3c6377077a1848c5947a4e6e8da6
Jan Buschtöns <jan.buschtoens@clark.de>
2018-10-23 00:50:45 +0200

    refactor(debounce): increase type safety

--- addon/debounce.ts
+++ addon/debounce.ts
@@ -2,18 +2,23 @@ import { decoratorWithRequiredParams } from '@ember-decorators/utils/decorator';
 import EmberObject from '@ember/object';
 import { debounceTask } from 'ember-lifeline';
 import hookDisposablesRunner from './hook-disposables-runner';
+import { PropertiesOfType } from './utils/type-helpers';
 
-export default decoratorWithRequiredParams(function(
-  target: EmberObject,
-  _key: string,
+export default decoratorWithRequiredParams(function<
+  O extends EmberObject,
+  K extends PropertiesOfType<O, (...args: any[]) => any>,
+  OriginalMethod extends Extract<O[K], (...args: any[]) => any>
+>(
+  target: O,
+  _key: K,
   desc: PropertyDescriptor,
   [wait, immediate = false]: [number, boolean?]
 ) {
   hookDisposablesRunner(target);
 
   if (desc) {
-    const originalMethod = desc.value;
-    desc.value = function(this: EmberObject, ...args: any[]) {
+    const originalMethod: OriginalMethod = desc.value;
+    desc.value = function(this: O, ...args: Parameters<OriginalMethod>) {
       return debounceTask(this, originalMethod, ...args, wait, immediate);
     };
   }

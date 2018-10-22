a553fb5602f76804c2cec5a7a2b88329a52faec6
Jan Buschtöns <jan.buschtoens@clark.de>
2018-10-23 00:58:39 +0200

    refactor(schedule): increase type safety

--- addon/schedule.ts
+++ addon/schedule.ts
@@ -2,20 +2,25 @@ import { decoratorWithRequiredParams } from '@ember-decorators/utils/decorator';
 import EmberObject from '@ember/object';
 import { scheduleTask } from 'ember-lifeline';
 import hookDisposablesRunner from './hook-disposables-runner';
+import { PropertiesOfType } from './utils/type-helpers';
 
 type RunLoopQueue = Exclude<EmberRunQueues, 'afterRender'>;
 
-export default decoratorWithRequiredParams(function(
-  target: EmberObject,
-  _key: string,
+export default decoratorWithRequiredParams(function<
+  O extends EmberObject,
+  K extends PropertiesOfType<O, (...args: any[]) => any>,
+  OriginalMethod extends Extract<O[K], (...args: any[]) => any>
+>(
+  target: O,
+  _key: K,
   desc: PropertyDescriptor,
   [queue]: [RunLoopQueue]
 ) {
   hookDisposablesRunner(target);
 
   if (desc) {
-    const originalMethod = desc.value;
-    desc.value = function(this: EmberObject, ...args: any[]) {
+    const originalMethod: OriginalMethod = desc.value;
+    desc.value = function(this: O, ...args: Parameters<OriginalMethod>) {
       return scheduleTask(this, queue, originalMethod, ...args);
     };
   }

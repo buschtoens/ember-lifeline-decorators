b86052c5bde2369fbe64fbaa648bc343b7d113c3
Jan Buschtöns <jan.buschtoens@clark.de>
2018-10-23 00:54:28 +0200

    fix(event-listener): increase type safety

--- addon/event-listener.ts
+++ addon/event-listener.ts
@@ -2,15 +2,26 @@ import { decoratorWithRequiredParams } from '@ember-decorators/utils/decorator';
 import EmberObject from '@ember/object';
 import { addEventListener } from 'ember-lifeline';
 import hookDisposablesRunner from './hook-disposables-runner';
+import { PropertiesOfType } from './utils/type-helpers';
 
-export default decoratorWithRequiredParams(function(
-  target: EmberObject,
-  _key: string,
+export default decoratorWithRequiredParams(function<
+  O extends EmberObject,
+  K extends PropertiesOfType<O, (...args: any[]) => any>,
+  OriginalMethod extends Extract<O[K], (...args: any[]) => any>
+>(
+  target: O,
+  _key: K,
   desc: PropertyDescriptor,
-  [element, eventName, options]: [EventTarget, string, object?]
+  [eventTarget, eventName, options]: [EventTarget, string, object?]
 ) {
   hookDisposablesRunner(target);
-  // @ts-ignore typing is incorrect upstream
-  addEventListener(target, element, eventName, desc.value, options);
+  addEventListener(
+    target,
+    // @ts-ignore https://github.com/ember-lifeline/ember-lifeline/pull/249
+    eventTarget,
+    eventName,
+    desc.value as OriginalMethod,
+    options
+  );
   return desc;
 });

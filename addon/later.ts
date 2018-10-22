076c4df35c29bd7ab09ec6bf818676d958f8fc80
Jan Buschtöns <jan.buschtoens@clark.de>
2018-10-23 00:47:35 +0200

    fix(later): enable passing arguments and increase type safety

--- addon/later.ts
+++ addon/later.ts
@@ -2,19 +2,19 @@ import { decoratorWithRequiredParams } from '@ember-decorators/utils/decorator';
 import EmberObject from '@ember/object';
 import { runTask } from 'ember-lifeline';
 import hookDisposablesRunner from './hook-disposables-runner';
+import { PropertiesOfType } from './utils/type-helpers';
 
-export default decoratorWithRequiredParams(function(
-  target: EmberObject,
-  _key: string,
-  desc: PropertyDescriptor,
-  [timeout]: [number]
-) {
+export default decoratorWithRequiredParams(function<
+  O extends EmberObject,
+  K extends PropertiesOfType<O, (...args: any[]) => any>,
+  OriginalMethod extends Extract<O[K], (...args: any[]) => any>
+>(target: O, _key: K, desc: PropertyDescriptor, [timeout]: [number]) {
   hookDisposablesRunner(target);
 
   if (desc) {
-    const originalMethod = desc.value;
-    desc.value = function(this: EmberObject) {
-      return runTask(this, originalMethod, timeout);
+    const originalMethod: OriginalMethod = desc.value;
+    desc.value = function(this: O, ...args: Parameters<OriginalMethod>) {
+      return runTask(this, originalMethod.bind(this, ...args), timeout);
     };
   }
   return desc;

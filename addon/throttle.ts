c041f7d4174da42f9b2cb12c9964edb5a31e2827
Jan Buschtöns <jan.buschtoens@clark.de>
2018-10-23 00:57:14 +0200

    fix(throttle): enable args passing and increase type safety

--- addon/throttle.ts
+++ addon/throttle.ts
@@ -2,19 +2,24 @@ import { decoratorWithRequiredParams } from '@ember-decorators/utils/decorator';
 import EmberObject from '@ember/object';
 import { throttleTask } from 'ember-lifeline';
 import hookDisposablesRunner from './hook-disposables-runner';
+import { PropertiesOfType } from './utils/type-helpers';
 
-export default decoratorWithRequiredParams(function(
-  target: EmberObject,
-  _key: string,
+export default decoratorWithRequiredParams(function<
+  O extends EmberObject,
+  K extends PropertiesOfType<O, (...args: any[]) => any>,
+  OriginalMethod extends Extract<O[K], (...args: any[]) => any>
+>(
+  target: O,
+  _key: K,
   desc: PropertyDescriptor,
   [spacing, immediate = true]: [number, boolean?]
 ) {
   hookDisposablesRunner(target);
 
   if (desc) {
-    const originalMethod = desc.value;
-    desc.value = function(this: EmberObject, ...args: any[]) {
-      return throttleTask(this, originalMethod, ...args, spacing, immediate);
+    const originalMethod: OriginalMethod = desc.value;
+    desc.value = function(this: O, ...args: Parameters<O>) {
+      return throttleTask(this, originalMethod.bind(this, ...args), spacing, immediate);
     };
   }
   return desc;

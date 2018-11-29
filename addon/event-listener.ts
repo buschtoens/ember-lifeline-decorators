c413b56661dd8d8f74a69bcc0ff9d0a8b522d026
Jan Buschtöns <jan.buschtoens@clark.de>
2018-11-29 18:34:08 +0100

    feat(event-listener): allow passing callback for eventTarget

--- addon/event-listener.ts
+++ addon/event-listener.ts
@@ -6,15 +6,49 @@ import { addEventListener } from 'ember-lifeline';
 import hookDisposablesRunner from './hook-disposables-runner';
 import { assert } from '@ember/debug';
 import ANONYMOUS from './utils/anonymous-field';
+import EmberObject from '@ember/object';
+import { beforeMethod } from 'patch-method';
+import Component from '@ember/component';
+import { Constructor } from 'ts-std';
+
+function NOOP() {}
 
 export default decoratorWithRequiredParams(function(
   desc: MethodDescriptor,
-  [eventTarget, eventName, options]: [EventTarget, string, object?]
+  [eventTarget, eventName, options]: [
+    EventTarget | ((this: Component, obj: Component) => EventTarget),
+    string,
+    object?
+  ]
 ) {
   assert(
     `The '@eventListener' decorator can only be used on methods.`,
     desc.kind === 'method'
   );
+
+  if (typeof eventTarget === 'function') {
+    return {
+      ...desc,
+      finisher(Class: Constructor<EmberObject>) {
+        beforeMethod(
+          Class as Constructor<Component>,
+          Class.prototype instanceof Component ? 'didInsertElement' : 'init',
+          function() {
+            addEventListener(
+              this,
+              // @ts-ignore https://github.com/ember-lifeline/ember-lifeline/pull/249
+              eventTarget.call(this, this),
+              eventName,
+              desc.descriptor.value,
+              options
+            );
+          },
+          NOOP
+        );
+        hookDisposablesRunner(Class);
+      }
+    };
+  }
 
   return {
     ...desc,
@@ -35,7 +69,6 @@ export default decoratorWithRequiredParams(function(
           );
         }
       }
-    ],
-    finisher: hookDisposablesRunner
+    ]
   };
 });
